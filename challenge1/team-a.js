93b00443248757499f9dc8c864550f16a6c64e8b
Michael Puening <mpuening@gmail.com>
2020-11-02 00:51:09 -0500

    Recap challenge 1, setup challenge 2

--- challenge1/team-a.js
+++ challenge1/team-a.js
@@ -1,58 +1,34 @@
 const team_a = {
 	name : "Team A",
 	stepsGoal : 1000000,
-	daysInChallenge : 49,
+	daysInChallenge : 23,
 	steps : [ {
 		"Sunday" : 31148,
 		"Monday" : 31659,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Tuesday" : 42427,
+		"Wednesday" : 50408,
+		"Thursday" : 46012,
+		"Friday" : 45643,
+		"Saturday" : 29853
 	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Sunday" : 42182,
+		"Monday" : 53176,
+		"Tuesday" : 43132,
+		"Wednesday" : 41064,
+		"Thursday" : 51215,
+		"Friday" : 41399,
+		"Saturday" : 34011
 	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Sunday" : 44101,
+		"Monday" : 46118,
+		"Tuesday" : 51345,
+		"Wednesday" : 66324,
+		"Thursday" : 62171,
+		"Friday" : 50587,
+		"Saturday" : 40846
 	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
-	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
-	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
-	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
+		"Sunday" : 40570,
+		"Monday" : 45315,
 		"Tuesday" : undefined,
 		"Wednesday" : undefined,
 		"Thursday" : undefined,
---
+++ challenge2/team-a.js
@@ -0,0 +1,62 @@
+const team_a = {
+	name : "Team A",
+	stepsGoal : 1000000,
+	daysInChallenge : 50,
+	steps : [ {
+		"Sunday" : 21241,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	}, {
+		"Sunday" : undefined,
+		"Monday" : undefined,
+		"Tuesday" : undefined,
+		"Wednesday" : undefined,
+		"Thursday" : undefined,
+		"Friday" : undefined,
+		"Saturday" : undefined
+	} ]
+};
