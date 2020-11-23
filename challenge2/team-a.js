235b3e9fb244491606d38da47753bd2073362cb6
Michael Puening <mpuening@gmail.com>
2020-11-22 23:55:54 -0500

    Update on challenge #2

--- challenge2/team-a.js
+++ challenge2/team-a.js
@@ -4,30 +4,30 @@ const team_a = {
 	daysInChallenge : 50,
 	steps : [ {
 		"Sunday" : 21241,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Monday" : 25107,
+		"Tuesday" : 24043,
+		"Wednesday" : 20035,
+		"Thursday" : 25512,
+		"Friday" : 20841,
+		"Saturday" : 32853
 	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Sunday" : 25062,
+		"Monday" : 12647,
+		"Tuesday" : 23284,
+		"Wednesday" : 22021,
+		"Thursday" : 20591,
+		"Friday" : 22012,
+		"Saturday" : 22298
 	}, {
-		"Sunday" : undefined,
-		"Monday" : undefined,
-		"Tuesday" : undefined,
-		"Wednesday" : undefined,
-		"Thursday" : undefined,
-		"Friday" : undefined,
-		"Saturday" : undefined
+		"Sunday" : 22222,
+		"Monday" : 27024,
+		"Tuesday" : 20668,
+		"Wednesday" : 22122,
+		"Thursday" : 21009,
+		"Friday" : 22554,
+		"Saturday" : 21233
 	}, {
-		"Sunday" : undefined,
+		"Sunday" : 13240,
 		"Monday" : undefined,
 		"Tuesday" : undefined,
 		"Wednesday" : undefined,
