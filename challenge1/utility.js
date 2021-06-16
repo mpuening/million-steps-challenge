f3ae9b072d0aa57fa103c8712856cc94c02ae778
Michael Puening <mpuening@gmail.com>
2021-06-16 01:04:41 -0400

    Support for older browsers

--- challenge1/utility.js
+++ challenge1/utility.js
@@ -84,79 +84,89 @@ class Team {
 	}
 
 	applyStepsGraphToDiv(divId) {
-		// labels: day numbers
-		const labels = [...Array(this.team.daysInChallenge).keys()].map(i => i + 1);
-
-		const stepGoalSeries = [...Array(this.team.daysInChallenge).keys()].map(i => this.team.stepsGoal);
-		const stepSeries = [];
-
-		const days = this.days;
-		var totalSteps = 0;
-		$.each(this.team.steps, function(w, week) {
-			$.each(days, function(d, day) {
-				var stepsPerDay = week[day];
-				if (stepsPerDay) {
-					totalSteps += stepsPerDay || 0;
-					stepSeries.push(totalSteps);
+		try {
+			// labels: day numbers
+			const labels = [...Array(this.team.daysInChallenge).keys()].map(i => i + 1);
+	
+			const stepGoalSeries = [...Array(this.team.daysInChallenge).keys()].map(i => this.team.stepsGoal);
+			const stepSeries = [];
+	
+			const days = this.days;
+			var totalSteps = 0;
+			$.each(this.team.steps, function(w, week) {
+				$.each(days, function(d, day) {
+					var stepsPerDay = week[day];
+					if (stepsPerDay) {
+						totalSteps += stepsPerDay || 0;
+						stepSeries.push(totalSteps);
+					}
+				});
+			});
+			
+			new Chartist.Line(divId, {
+				labels: labels,
+				series: [
+					stepGoalSeries, stepSeries 
+				]
+			}, {
+				fullWidth: false,
+				chartPadding: {
+					left: 10,
+					right: 100
 				}
 			});
-		});
-		
-		new Chartist.Line(divId, {
-			labels: labels,
-			series: [
-				stepGoalSeries, stepSeries 
-			]
-		}, {
-			fullWidth: false,
-			chartPadding: {
-				left: 10,
-				right: 100
-			}
-		});
+		}
+		catch (err) {
+			$(divId).html("Graphs not supported.");
+		}
 		return this;
 	}
 
 	applyAverageGraphToDiv(divId) {
-		// labels: day numbers
-		var labels = [...Array(this.team.daysInChallenge).keys()].map(i => i + 1);
-
-		var dailyRemainingAvg = [];
-
-		const days = this.days;
-		const stepsGoal = this.team.stepsGoal;
-		const daysInChallenge = this.team.daysInChallenge;
-		var totalSteps = 0;
-		var daysWalked = 0;
-		$.each(this.team.steps, function(w, week) {
-			$.each(days, function(d, day) {
-				var stepsPerDay = week[day];
-				daysWalked += (stepsPerDay != undefined) ? 1 : 0;
-				if (stepsPerDay) {
-					totalSteps += stepsPerDay || 0;
-					var remaining = stepsGoal - totalSteps;
-					if (remaining > 0) {
-						dailyRemainingAvg.push(remaining / (daysInChallenge - daysWalked));
+		try {
+			// labels: day numbers
+			var labels = [...Array(this.team.daysInChallenge).keys()].map(i => i + 1);
+	
+			var dailyRemainingAvg = [];
+	
+			const days = this.days;
+			const stepsGoal = this.team.stepsGoal;
+			const daysInChallenge = this.team.daysInChallenge;
+			var totalSteps = 0;
+			var daysWalked = 0;
+			$.each(this.team.steps, function(w, week) {
+				$.each(days, function(d, day) {
+					var stepsPerDay = week[day];
+					daysWalked += (stepsPerDay != undefined) ? 1 : 0;
+					if (stepsPerDay) {
+						totalSteps += stepsPerDay || 0;
+						var remaining = stepsGoal - totalSteps;
+						if (remaining > 0) {
+							dailyRemainingAvg.push(remaining / (daysInChallenge - daysWalked));
+						}
+						else {
+							dailyRemainingAvg.push(0);
+						}
 					}
-					else {
-						dailyRemainingAvg.push(0);
-					}
+				});
+			});
+			
+			new Chartist.Line(divId, {
+				labels: labels,
+				series: [
+					dailyRemainingAvg
+				]
+			}, {
+				fullWidth: false,
+				chartPadding: {
+					left: 10,
+					right: 100
 				}
 			});
-		});
-		
-		new Chartist.Line(divId, {
-			labels: labels,
-			series: [
-				dailyRemainingAvg
-			]
-		}, {
-			fullWidth: false,
-			chartPadding: {
-				left: 10,
-				right: 100
-			}
-		});
+		}
+		catch (err) {
+			$(divId).html("Graphs not supported.");
+		}
 		return this;
 	}
 }
